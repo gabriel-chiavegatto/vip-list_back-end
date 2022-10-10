dff0ba5463cac34b792bc83d90b8f0dc8cae5499
Gabriel Chiavegatto <chiavegattogomes@gmail.com>
2022-10-10 01:12:30 -0300

    create a server

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from "dotenv"; dotenv.config();
+
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, ()=>{console.log(`Server run in ${PORT}`)});
